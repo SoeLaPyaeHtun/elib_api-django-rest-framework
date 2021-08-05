e5c3be77959e8af06d339a1123bf8611b4a651c7
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2021-08-06 01:24:23 +0630

    setup media and static

--- src/elib_api/models.py
+++ src/elib_api/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Author(models.Model):
     pen_name = models.CharField(max_length=64, unique=True)
     name = models.CharField(max_length=64, blank=True, null=True)
-    date_of_birth = models.DateTimeField(blank=True)
+    date_of_birth = models.DateField(blank=True)
     photo = models.ImageField(
-        upload_to="src/photo", blank=True, null=True
+        upload_to="author_img/", default="author_img/defaul.jpg"
     )
+
+    def __str__(self):
+        return self.pen_name
