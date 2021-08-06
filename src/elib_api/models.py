275c6e05eff36a0a2655ed22965f3d75260d2baa
soelapyaehtun <soelapyaehtun@gmail.com>
2021-08-06 13:33:29 +0630

    create books and category models

--- src/elib_api/models.py
+++ src/elib_api/models.py
@@ -13,3 +13,25 @@ class Author(models.Model):
 
     def __str__(self):
         return self.pen_name
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.category
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    author = models.ForeignKey(Author,blank=True, null=True, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category,blank=True, null=True)
+    cover_img = models.ImageField(
+        upload_to="book_cover_img/", default="book_cover_img/default.jpg"
+    )
+    file = models.FileField(
+        upload_to="book_file/",blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.title    
