ade10cc826cef6b92809f5ae4db427cb5682067f
soelapyaehtun <soelapyaehtun@gmail.com>
2021-08-06 15:26:59 +0630

    some update admin.py

--- src/elib_api/admin.py
+++ src/elib_api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Author, Category, Book
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    filter_horizontal = ("category",)
+
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Book)
+admin.site.register(Book,BookAdmin)
